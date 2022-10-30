420843223f6c9f43b830f1110102e45732e05570
Ekaterina Donets <116032996+KDonec@users.noreply.github.com>
2022-10-30 14:58:06 +0300

    Create Lection-06.java

---
+++ Lection-06.java
@@ -0,0 +1,21 @@
+// Задача 1 
+
+import java.util.*;
+
+public class Main {
+    public static void print() {
+     for(int i = 0; i < 4; i++) {
+      System.out.println("5 5 5 5 5 5");
+      } 
+    }
+    
+    public static void main(String[] args) {
+      System.out.println("Задача 6-1");
+      print(); 
+      
+  }
+}
+
+
+// Задача 2
+
